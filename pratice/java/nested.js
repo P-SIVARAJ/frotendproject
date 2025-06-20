faeb4aa8e8b17f2acadc133ed1c2a0d4433b2696
sivaraj <sivarajp612@gmail.com>
2025-06-20 12:25:48 +0530

    sdiduddj.12

---
+++ pratice/java/nested.js
@@ -0,0 +1,10 @@
+function sub(){
+    let value = document.querySelector(".follow");
+    if(value.innerText=="follow"){
+      value.innerText="following";
+    }
+    else{
+        value.innerText="follow";
+    }
+    console.log("siva");
+}
